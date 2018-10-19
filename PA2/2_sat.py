2e0a2e5fc78ddde8316ed7eb8c46c77a8359a04a
rachitrawat <rachit.rc.rawat@gmail.com>
2018-10-19 16:23:59 +0530

    modularize

--- PA2/2_sat.py
+++ PA2/2_sat.py
@@ -1,48 +1,80 @@
 import random
 
-# n boolean variables
-n = random.randint(3, 5)
-# m clauses
-m = random.randint(3, 5)
-print("Number of variables:", n)
-print("Number of clauses:", m)
 
-exp = []
+def generate_random_exp():
+    # n boolean variables
+    n = random.randint(3, 5)
+    # m clauses
+    m = random.randint(3, 5)
+    print("Number of variables:", n)
+    print("Number of clauses:", m)
 
-for i in range(1, m + 1):
-    x = random.randint(0, n - 1)
-    y = random.randint(0, n - 1)
+    exp = []
 
-    if random.randint(0, 1) == 0:
-        x = -1 * x
+    for i in range(0, m):
+        x = random.randint(0, n - 1)
+        y = random.randint(0, n - 1)
 
-    if random.randint(0, 1) == 0:
-        y = -1 * y
+        if random.randint(0, 1) == 0:
+            x = -1 * x
 
-    exp.append([x, y])
+        if random.randint(0, 1) == 0:
+            y = -1 * y
 
-print("\nExpression:", exp)
+        exp.append([x, y])
 
+    print("Generated Expression:", exp)
+    return n, exp
+
+
+def evaluate_exp(exp, assignment):
+    print("\nExpression:", exp)
+    print("Assignment:", assignment)
+
+    unsatisfied_clauses_idx = []
+    unsatisfied_clauses = []
+    exp_eval = 1
+    for idx, clause in enumerate(exp):
+        x = assignment[abs(clause[0])]
+        y = assignment[abs(clause[1])]
+
+        # if negation, invert truth value
+        if clause[0] < 0:
+            x = (x + 1) % 2
+        if clause[1] < 0:
+            y = (y + 1) % 2
+
+        print("\nx%s: %s" % (clause[0], x))
+        print("y%s: %s" % (clause[1], y))
+
+        clause_eval = x + y
+        print("Clause Eval:", clause_eval)
+        exp_eval *= clause_eval
+        print("Exp Eval:", exp_eval)
+        print()
+
+        if clause_eval == 0:
+            unsatisfied_clauses_idx.append(idx)
+            unsatisfied_clauses.append(clause)
+
+    if exp_eval != 0:
+        print("Satisfying Assginment: ", assignment)
+        return True, assignment
+
+    # print("Unsatisfied clauses Index: ", unsatisfied_clauses_idx),
+    print("Unsatisfied clauses: ", unsatisfied_clauses),
+    return False, unsatisfied_clauses_idx
+
+
+n, exp = generate_random_exp()
 # initial assignment 1
 assignment = [1] * n
-print("Assignment:", assignment)
+bool, x = evaluate_exp(exp, assignment)
 
-exp_eval = 1
-for clause in exp:
-    x = assignment[abs(clause[0])]
-    y = assignment[abs(clause[1])]
-
-    # if negation, invert truth value
-    if clause[0] < 0:
-        x = (x + 1) % 2
-    if clause[1] < 0:
-        y = (y + 1) % 2
-
-    print("\nx%s: %s" % (clause[0], x))
-    print("y%s: %s" % (clause[1], y))
-
-    clause_eval = x + y
-    print("Clause Eval:", clause_eval)
-    exp_eval *= clause_eval
-    print("Exp Eval:", exp_eval)
-    print()
+while not bool:
+    random_clause = x[random.randint(0, len(x) - 1)]
+    print("Random Clause:", exp[random_clause])
+    random_literal = exp[random_clause][random.randint(0, 1)]
+    print("Random Literal:", random_literal)
+    assignment[abs(random_literal)] = (assignment[abs(random_literal)] + 1) % 2
+    bool, x = evaluate_exp(exp, assignment)
