5006d4540041cfc29fad5f2c88ce61f0bab30981
rachitrawat <rachit.rc.rawat@gmail.com>
2018-10-19 17:12:23 +0530

    sync

--- PA2/st_dir.py
+++ PA2/st_dir.py
@@ -1,6 +1,9 @@
 from PA2.modules import graph
 
 G = graph.createRandomUDGraph()
+v = len(G)
+threshold = 2 * len(G) ** 3
+print("\nThreshold:", threshold)
 
 s = int(input("\nEnter s:"))
 t = int(input("Enter t:"))
