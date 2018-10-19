4ec309d32967592be7ffa7809ccd4b2914362953
rachitrawat <rachit.rc.rawat@gmail.com>
2018-10-19 15:54:42 +0530

    exp eval

--- PA2/2_sat.py
+++ PA2/2_sat.py
@@ -9,9 +9,9 @@ print("Number of clauses:", m)
 
 exp = []
 
-for i in range(1, m):
-    x = random.randint(1, n + 1)
-    y = random.randint(1, n + 1)
+for i in range(1, m + 1):
+    x = random.randint(0, n - 1)
+    y = random.randint(0, n - 1)
 
     if random.randint(0, 1) == 0:
         x = -1 * x
@@ -21,4 +21,28 @@ for i in range(1, m):
 
     exp.append([x, y])
 
-print("Expression:", exp)
+print("\nExpression:", exp)
+
+# initial assignment 1
+assignment = [1] * n
+print("Assignment:", assignment)
+
+exp_eval = 1
+for clause in exp:
+    x = assignment[abs(clause[0])]
+    y = assignment[abs(clause[1])]
+
+    # if negation, invert truth value
+    if clause[0] < 0:
+        x = (x + 1) % 2
+    if clause[1] < 0:
+        y = (y + 1) % 2
+
+    print("\nx%s: %s" % (clause[0], x))
+    print("y%s: %s" % (clause[1], y))
+
+    clause_eval = x + y
+    print("Clause Eval:", clause_eval)
+    exp_eval *= clause_eval
+    print("Exp Eval:", exp_eval)
+    print()
