3f0e8f59f44781512f6c59a0e804912cb4c0943c
rachitrawat <rachit.rc.rawat@gmail.com>
2018-10-19 12:35:31 +0530

    modularize

--- PA2/min_cut.py
+++ PA2/min_cut.py
@@ -3,6 +3,94 @@ import pprint
 import random
 
 
+def createRandomGraph():
+    # no of vertices
+    v = random.randint(3, 6)
+
+    print("No of vertices: ", v)
+
+    G = {}
+    visited = []
+
+    # create a dummy graph
+    for i in range(1, v + 1):
+        if i not in G:
+            G[i] = {}
+        for j in range(1, v + 1):
+            if j not in G[i]:
+                G[i][j] = 0
+
+    m = 0
+    for vertex1 in range(1, v + 1):
+        for vertex2 in range(1, v + 1):
+            if vertex2 != vertex1 and {vertex1, vertex2} not in visited:
+                r = random.randint(0, 5)
+                G[vertex1][vertex2] = r
+                G[vertex2][vertex1] = r
+                m += r
+                visited.append({vertex1, vertex2})
+
+    print("No of edges: ", m)
+    print()
+    pprint.pprint(G)
+    return G
+
+
+def selectRandomEdge(G):
+    degree = []
+    v = len(G)
+
+    # cumulative degree
+    for i in range(1, v + 1):
+        degree.append(0)
+        for j in range(1, v + 1):
+            degree[i - 1] += (G[i][j])
+        if i != 1:
+            degree[i - 1] += degree[i - 2]
+
+    print("\nCumulative degree set 1:", degree)
+    r = random.randint(1, degree[len(degree) - 1])
+    print("Random Number:", r)
+    index1 = binarySearch(degree, 0, len(degree) - 1, r)
+    vertex1 = index1 + 1
+    print("Corresponding Vertex:", vertex1)
+
+    degree = []
+    neighbours = []
+    index_vertex_map = {}
+
+    for k, v in G[vertex1].items():
+        if v != 0:
+            neighbours.append(k)
+
+    print("\nNeighbours of Vertex %s: %s" % (vertex1, neighbours))
+
+    # cumulative degree
+
+    i = 0
+    for k, v in G[vertex1].items():
+        sum = 0
+        if k in neighbours:
+            for k2, v2 in G[k].items():
+                sum += v2
+            degree.append(sum)
+            index_vertex_map[i] = k
+            if i != 0:
+                degree[i] += degree[i - 1]
+            i += 1
+
+    print("Cumulative degree set 2:", degree)
+    print("Index to Vertex Map:", index_vertex_map)
+    r = random.randint(1, degree[len(degree) - 1])
+    print("Random Number:", r)
+    index2 = binarySearch(degree, 0, len(degree) - 1, r)
+    vertex2 = index_vertex_map[index2]
+    print("Corresponding Vertex:", vertex2)
+
+    print("\nUniformly selected edge: ", vertex1, vertex2)
+    return vertex1, vertex2
+
+
 def binarySearch(a, l, r, x):
     length = len(a)
 
@@ -26,82 +114,5 @@ def binarySearch(a, l, r, x):
             return binarySearch(a, mid + 1, r, x)
 
 
-# no of vertices
-v = random.randint(3, 6)
-
-print("No of vertices: ", v)
-
-G = {}
-visited = []
-degree = []
-
-# create a dummy graph
-for i in range(1, v + 1):
-    if i not in G:
-        G[i] = {}
-    for j in range(1, v + 1):
-        if j not in G[i]:
-            G[i][j] = 0
-
-m = 0
-for vertex1 in range(1, v + 1):
-    for vertex2 in range(1, v + 1):
-        if vertex2 != vertex1 and {vertex1, vertex2} not in visited:
-            r = random.randint(0, 5)
-            G[vertex1][vertex2] = r
-            G[vertex2][vertex1] = r
-            m += r
-            visited.append({vertex1, vertex2})
-
-print("No of edges: ", m)
-print()
-pprint.pprint(G)
-
-# cumulative degree
-for i in range(1, v + 1):
-    degree.append(0)
-    for j in range(1, v + 1):
-        degree[i - 1] += (G[i][j])
-    if i != 1:
-        degree[i - 1] += degree[i - 2]
-
-print("\nCumulative degree set 1:", degree)
-r = random.randint(1, degree[len(degree) - 1])
-print("Random Number:", r)
-index1 = binarySearch(degree, 0, len(degree) - 1, r)
-vertex1 = index1 + 1
-print("Corresponding Vertex:", vertex1)
-
-degree = []
-neighbours = []
-index_vertex_map = {}
-
-for k, v in G[vertex1].items():
-    if v != 0:
-        neighbours.append(k)
-
-print("\nNeighbours of Vertex %s: %s" % (vertex1, neighbours))
-
-# cumulative degree
-
-i = 0
-for k, v in G[vertex1].items():
-    sum = 0
-    if k in neighbours:
-        for k2, v2 in G[k].items():
-            sum += v2
-        degree.append(sum)
-        index_vertex_map[i] = k
-        if i != 0:
-            degree[i] += degree[i - 1]
-        i += 1
-
-print("Cumulative degree set 2:", degree)
-print("Index to Vertex Map:", index_vertex_map)
-r = random.randint(1, degree[len(degree) - 1])
-print("Random Number:", r)
-index2 = binarySearch(degree, 0, len(degree) - 1, r)
-vertex2 = index_vertex_map[index2]
-print("Corresponding Vertex:", vertex2)
-
-print("\nUniformly selected edge: ", vertex1, vertex2)
+G = createRandomGraph()
+v1, v2 = selectRandomEdge(G)
