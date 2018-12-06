bf7ba7a7623553f0475f474782230814d4ca6886
rachitrawat <rachit.rc.rawat@gmail.com>
2018-12-06 15:41:45 +0530

    optimizations

--- PA4/cp1.py
+++ PA4/cp1.py
@@ -3,7 +3,7 @@ from _operator import itemgetter
 
 
 def dist(p1, p2):
-    return math.sqrt(pow(p2[0] - p1[0], 2) + pow(p2[1] - p1[1], 2))
+    return pow(p2[0] - p1[0], 2) + pow(p2[1] - p1[1], 2)
 
 
 def bruteForce(P, n):
@@ -15,21 +15,21 @@ def bruteForce(P, n):
             if d < min_:
                 min_ = d
                 pair = [P[i], P[j]]
-    return min_, pair
+    return math.sqrt(min_), pair
 
 
 def closestStrip(strip, size, d):
-    min_ = d
+    min_ = pow(d, 2)
     strip = sorted(strip, key=itemgetter(1))
     pair = []
     for i in range(0, size):
-        for j in range(i + 1, size):
+        for j in range(i + 1, min(size, i + 6)):
             if abs(strip[j][1] - strip[i][1]) < min_:
                 d = dist(strip[i], strip[j])
                 if d < min_:
                     min_ = d
                     pair = [strip[i], strip[j]]
-    return min_, pair
+    return math.sqrt(min_), pair
 
 
 def closestUtil(P, n):
